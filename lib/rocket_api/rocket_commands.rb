a2f30c6aa9c74ac4fcab9f0757fda4a8ea76c2f8
kilya <ilya.kondratyev@kupibilet.ru>
2022-06-17 22:34:04 +0300

    [0000] fix errors

--- lib/rocket_api/rocket_commands.rb
+++ lib/rocket_api/rocket_commands.rb
@@ -1,4 +1,7 @@
-require "rocket_api/rocket_commands"
+require "rocket_api/commands/files"
+require "rocket_api/commands/dirs"
+require "rocket_api/commands/gems_dir"
+require "rocket_api/library/gem_repo_plain_text"
 
 module RocketApi
   class RocketCommands
