b37a22908dfc283b70edc9d3eada9f0259f9ffe3
kilya <ilya.kondratyev@kupibilet.ru>
2022-06-21 18:50:18 +0300

    [0011] Add rubocop.yml file

--- lib/rocket_api/commands/gems_dir.rb
+++ lib/rocket_api/commands/gems_dir.rb
@@ -27,6 +27,11 @@ module RocketApi
         file_name = "test/test_version.rb"
         create_single_file(file_name, plain_gem_test_version_text(name, class_name_camel(name)))
       end
+
+      def rubocop_yml!(name)
+        file_name = ".rubocop.yml"
+        create_single_file(file_name, plain_rubocop_yml_text(name))
+      end
     end
   end
 end
