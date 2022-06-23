7ca24889000ddb34a6d7f4bdc744caa696fcb466
kilya <ilya.kondratyev@kupibilet.ru>
2022-06-23 22:22:19 +0300

    [0015] Add new commands

--- lib/rocket_api/constants.rb
+++ lib/rocket_api/constants.rb
@@ -1,13 +1,22 @@
 module RocketApi
   # Valid input
   REGEXP_VALID = /[^0-9A-Za-z_-]/.freeze
+  CHECK_APPS = %w[*.gemspec *.ru].freeze
 
   # Dirs map
-  GEM_PROJECTS_DIR = %w[bin lib test].freeze
-  GEM_COMMANDS = %w[bin! gemspec! gems_main_file! gems_version! gem_test! rake_file! gitignore! gem_file! rubocop_yml!].freeze
+  GEM_DIRS = %w[bin lib test].freeze
+  GEM_FILES = %w[bin! gemspec! gems_main_file! gems_version! gem_test! rake_file! gitignore! gem_file! rubocop_yml!].freeze
+  RACK_DIRS = %w[app config lib].freeze
   AVAILABLE_COMMANDS = {
     init: {
-      gem: %w[init_gem_dir init_gem_files]
+      gem: {
+        init_dirs: GEM_DIRS,
+        init_files: GEM_FILES
+      },
+      rack_app: {
+        init_dirs: RACK_DIRS,
+        init_files: []
+      }
     }
   }.freeze
 
@@ -16,7 +25,7 @@ module RocketApi
 
   # Answers
   INIT_DIR_ERROR = "Init directory error".freeze
-  GEM_DETECTED = "another one gem exist".freeze
+  APP_DETECTED = "another one app exist".freeze
   FOLDER_EXIST = "folder exist:".freeze
   FILE_EXIST = "file already exist:".freeze
   WRONG_RESPONSE = "Wrong command".freeze
